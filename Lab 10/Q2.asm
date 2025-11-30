af695074810ccb8a5cd87f36b5c71510e63a2686
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:12:38 +0500

    Update Q2.asm

--- Lab 10/Q2.asm
+++ Lab 10/Q2.asm
@@ -3,69 +3,84 @@
 ; by calling another function Display (Also show ESP values during nested function calls)
 TITLE Q2 (Q2.asm)
 INCLUDE Irvine32.inc
+
 .data
-    prompt BYTE "Enter two numbers: ",0
-    outmsg BYTE "GCD = ",0
-    espmsg BYTE "ESP in GCD: ",0
+    prompt  BYTE "Enter two numbers: ",0
+    outmsg  BYTE "GCD = ",0
+    espmsg  BYTE "ESP = ",0
 
 .code
+
 ShowESP PROC
     push ebp
-    mov ebp, esp
-    mov eax, esp
-    mov edx, OFFSET espmsg
+    mov  ebp, esp
+    mov  eax, esp
+    mov  edx, OFFSET espmsg
     call WriteString
     call WriteHex
     call Crlf
-    pop ebp
+    pop  ebp
     ret
 ShowESP ENDP
 
+TakeInput PROC
+    push ebp
+    mov  ebp, esp
+
+    call ShowESP
+
+    mov  edx, OFFSET prompt
+    call WriteString
+
+    call ReadInt
+    mov  esi, eax        ; first number
+
+    call ReadInt
+    mov  edi, eax        ; second number
+
+    pop  ebp
+    ret
+TakeInput ENDP
+
 GCD PROC
     push ebp
-    mov ebp, esp
-    mov eax, esp
-    mov edx, OFFSET espmsg
-    call WriteString
-    call WriteHex
-    call Crlf
+    mov  ebp, esp
+    mov  eax, esp
+    mov  edx, OFFSET espmsg
+    call ShowESP
 
-    mov eax, [ebp+8]   ; a
-    mov ebx, [ebp+12]  ; b
+    mov  eax, [ebp+8]   ; a
+    mov  ebx, [ebp+12]  ; b
 
 gcd_loop:
-    cmp ebx, 0
-    je gcd_done
-    xor edx, edx
-    mov ecx, eax
+    cmp  ebx, 0
+    je   gcd_done
+    xor  edx, edx
+    mov  ecx, eax
     cdq
-    div ebx
-    mov eax, ebx
-    mov ebx, edx
-    jmp gcd_loop
+    div  ebx
+    mov  eax, ebx
+    mov  ebx, edx
+    jmp  gcd_loop
 
 gcd_done:
-    mov edx, OFFSET outmsg
+    mov  edx, OFFSET outmsg
     call WriteString
     call WriteInt
     call Crlf
 
-    pop ebp
+    pop  ebp
     ret
 GCD ENDP
 
 main PROC
-    mov edx, OFFSET prompt
-    call WriteString
+    call TakeInput
 
-    call ReadInt
-    push eax
-    call ReadInt
-    push eax
-
-    call ShowESP
+    push edi
+    push esi
     call GCD
-    add esp, 8
+    add  esp, 8
+
     exit
 main ENDP
 
