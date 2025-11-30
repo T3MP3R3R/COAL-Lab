924900046be1ac9b2cb66f07f0cffdd9dcc6cceb
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:43:52 +0500

    Update Q8.asm

--- Lab 9/Q8.asm
+++ Lab 9/Q8.asm
@@ -1,3 +1,12 @@
+; Write a program that performs simple encryption by rotating each plaintext byte a varying
+; number of positions in different directions. For example, in the following array that represents the
+; encryption key, a negative value indicates a rotation to the left and a positive value indicates a
+; rotation to the right. The integer in each position indicates the magnitude of the rotation:
+; key BYTE 2, 4, 1, 0, 3, 5, 2, 4, 4, 6 program to write.
+; One possible approach is to use a loop to shift the multiplier to the right, keeping
+; track of the number of shifts that occur before the Carry flag is set. The resulting shift count can
+; then be applied to the SHR instruction, using the multiplicand as the destination operand. Then,
+; the same process must be repeated until you find the next highest bit in the multiplier.
 TITLE Q8 (Q8.asm)
 INCLUDE Irvine32.inc
 
@@ -40,4 +49,5 @@ done:
     call WriteString
     exit
 main ENDP
+
 END main
