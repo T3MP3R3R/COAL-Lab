527a4b1f17ce09420bd98e5bcfb7027de77575c0
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:39:17 +0500

    Update Q4A.asm

--- Lab 9/Q4A.asm
+++ Lab 9/Q4A.asm
@@ -1,19 +1,25 @@
 ; Write a series of instructions that shift the lowest bit of AX into the highest bit of BX
-; using the SHRD instruction.
-TITLE Q4 With SHRD (Q4.asm)
+; without using the SHRD instruction.
+TITLE Q4 Without SHRD (Q4.asm)
 INCLUDE Irvine32.inc
 
 .data
-Val1 WORD 1001h ; example values
+Val1 WORD 1001h
 Val2 WORD 1234h
 
 .code
 main PROC
     mov ax, Val1
     mov bx, Val2
-    shrd bx, ax, 1
+
+    shr bx, 1 ; shift bx right by 1
+    mov cx, ax
+    shl cx, 15 ; take the least significant bit of ax and move to MSB of cx
+    or  bx, cx ; combine
+
     mov Val1, ax
     mov Val2, bx
+
     movzx eax, bx
     call WriteHex
     call Crlf
