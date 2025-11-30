ae0a254a78a52103651a506e39843fc9f438a650
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:45:31 +0500

    Update Q9.asm

--- Lab 9/Q9.asm
+++ Lab 9/Q9.asm
@@ -1,3 +1,11 @@
+; Write a procedure named BitwiseMultiply that multiplies any unsigned 32-bit integer by EAX,
+; using only shifting and addition. Pass the integer to the procedure in the EBX register, and return
+; the product in the EAX register. Write a short test program that calls the procedure and displays
+; the product. (We will assume that the product is never larger than 32 bits.) 
+; One possible approach is to use a loop to shift the multiplier to the
+; right, keeping track of the number of shifts that occur before the Carry flag is set. The resulting
+; shift count can then be applied to the SHR instruction, using the multiplicand as the destination
+; operand. Then, the same process must be repeated until you find the next highest bit in the multiplier.
 TITLE Q9 (Q9.asm)
 INCLUDE Irvine32.inc
 
@@ -31,4 +39,5 @@ main PROC
     call Crlf
     exit
 main ENDP
+
 END main
