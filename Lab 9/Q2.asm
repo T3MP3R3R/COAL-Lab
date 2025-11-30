a78887a4acd7e46c49d00819a1d7bde86015b528
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:36:05 +0500

    Update Q2.asm

--- Lab 9/Q2.asm
+++ Lab 9/Q2.asm
@@ -1,3 +1,4 @@
+; Give an assembly language program to move -128 in ax and expand eax. Using shift and rotate instruction.
 TITLE Q2 (Q2.asm)
 INCLUDE Irvine32.inc
 
@@ -16,4 +17,5 @@ main PROC
 
     exit
 main ENDP
+
 END main
