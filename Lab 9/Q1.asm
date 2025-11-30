4fe08dba12bb82833dcbca8f983fe10cdd77c571
Salman A. <wanderingconjuror219696@gmail.com>
2025-11-30 20:33:14 +0500

    Update Q1.asm

--- Lab 9/Q1.asm
+++ Lab 9/Q1.asm
@@ -1,3 +1,5 @@
+; Write ASM instructions that calculate EAX * 21 using binary multiplication
+; Hint: 21 = 2^4+ 2^2+ 2^0
 TITLE Q1 (Q1.asm)
 INCLUDE Irvine32.inc
 
@@ -15,4 +17,5 @@ main PROC
     call Crlf
     exit
 main ENDP
+
 END main
