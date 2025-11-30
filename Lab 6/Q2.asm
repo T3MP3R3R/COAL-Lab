994a2ebfba4fdf65015f8de7bc9d9fdb089b2d3a
Salman A. <wanderingconjuror219696@gmail.com>
2025-12-01 03:40:00 +0500

    Update Q2.asm

--- Lab 6/Q2.asm
+++ Lab 6/Q2.asm
@@ -1,3 +1,7 @@
+; Write a program that prints numbers from 1 to 10 using a LOOP instruction.
+; Use ECX as the counter.
+; Use WriteInt to display each number.
+; Print each number on a new line with Crlf.
 TITLE Question 2 (Q2.asm)
 INCLUDE Irvine32.inc
 
@@ -17,4 +21,5 @@ L1:
 
     exit
 main ENDP
+
 END main
